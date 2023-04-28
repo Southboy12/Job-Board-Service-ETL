8c4e3a99769934d3d0cc14ac7f3023388f5e7ea8
Southboy12 <menkachi85@gmail.com>
2023-04-28 10:48:20 +0100

    A function to extract and write data to s3 bucket

---
+++ extract_raw_to_s3.py
@@ -0,0 +1,95 @@
+# Import the necessary libraries
+import requests
+import os
+import boto3, dotenv, json
+from datetime import datetime
+
+
+# Load the environment variables
+dotenv.load_dotenv('./.env')
+
+# Create an S3 client object
+s3_client = boto3.client('s3',
+                         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+
+# Define a function to extract data from the JSearch API
+def extract_data(folder):
+    # Define the API endpoint and parameters
+    url = "https://jsearch.p.rapidapi.com/search"
+
+    # Create a list of queries
+    query_list = [
+        {"query": "Data Engineer in USA", "page": "1", "num_pages": "1", "date_posted": "today"},
+        {"query": "Data Engineer in UK", "page": "1", "num_pages": "1", "date_posted": "today"},
+        {"query": "Data Engineer in Canada", "page": "1", "num_pages": "1", "date_posted": "today"},
+        {"query": "Data Analyst in USA", "page": "1", "num_pages": "1", "date_posted": "today"},
+        {"query": "Data Analyst in UK", "page": "1", "num_pages": "1", "date_posted": "today"},
+        {"query": "Data Analyst in Canada", "page": "1", "num_pages": "1", "date_posted": "today"}
+    ]
+
+    # Define the headers for the requests
+    headers = {
+        "content-type": "application/octet-stream",
+        "X-RapidAPI-Key": "4fce829339msh43b77f9802b5118p1e6aa8jsnbe60e04ba4e0",
+        "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+    }
+
+    # Make requests to the API and save responses to temporary files
+    temp_files = []
+    i = 1
+    for querystring in query_list:
+        # Make a request to the API
+        response = requests.get(url, headers=headers, params=querystring)
+
+        # Save the response to a temporary file
+        temp_file = f'test{i}.json'
+        with open(temp_file, 'w') as f:
+            f.write(response.text)
+
+        # Add the temporary file to the list of temporary files
+        temp_files.append(temp_file)
+
+        i += 1
+
+    # Merge the temporary files into a single file
+    merged_data = ''
+    for file in temp_files:
+        # Read the contents of the file
+        with open(file, 'r') as f:
+            merged_data += f.read()
+
+        # Remove the file
+        os.remove(file)
+
+    # Write the merged data to a local file
+    with open('merged-data.json', 'w') as f:
+        f.write(merged_data)
+
+    # Get the current date and time
+    current_date = datetime.now().strftime('%Y-%m-%d')
+
+    # Create the path to the file in S3
+    aws_path = f'{folder}/{current_date}'
+
+    # Upload the file to S3
+    s3_client.put_object(Bucket='jobsearch-data',
+                            Key=aws_path,
+                            Body=json.dumps(merged_data))
+
+    print('Success')
+
+
+# Define the main function
+def main():
+    # Define the folder where the data will be saved
+    folder = 'raw_data'
+
+    # Extract the data from the JSearch API
+    extract_data(folder)
+
+
+# If this is the main script, call the main function
+if __name__ == '__main__':
+    main()

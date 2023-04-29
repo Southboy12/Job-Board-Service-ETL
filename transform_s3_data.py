81c091f8ded0373dbd3d111ad0733ea91a456b52
Southboy12 <menkachi85@gmail.com>
2023-04-29 11:55:47 +0100

    Write transformed data back to another path in s3 bucket

--- transform_s3_data.py
+++ transform_s3_data.py
@@ -3,6 +3,7 @@ import boto3, dotenv, os
 from datetime import datetime
 from io import StringIO
 import io
+from pathlib import Path
 
 
 # Instantiate an s3 client object
@@ -23,13 +24,6 @@ def read_csv_from_s3(bucket_name: str, key_name: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The DataFrame containing the CSV file data.
     """
-    # response = s3_client.get_object(
-    #     Bucket=bucket_name,
-    #     Key=key_name
-    # )
-
-    # data = response['Body'].read().decode('utf-8')
-    # df = pd.read_csv(data)
 
     object_list = s3_client.list_objects(Bucket=bucket_name, Prefix=key_name)
     file = object_list.get('Contents')[0]
@@ -42,18 +36,28 @@ def read_csv_from_s3(bucket_name: str, key_name: str) -> pd.DataFrame:
 def transform(df):   
 
     df['job_posted_at_timestamp'] = pd.to_datetime(df.job_posted_at_timestamp)
-    print(df.info())
+    return df
+
+
+def write_to_s3(trans_df: pd.DataFrame, bucket_name: str, trans_key_name: str) -> Path:
+    trans_df.to_csv(trans_key_name, index=False)
+    s3_client.upload_file(trans_key_name, bucket_name, trans_key_name)
+    print('successful')
+    return trans_key_name
 
 
 
 def main():
     bucket_name = 'jobsearch-data'
     current_date = f"{datetime.now().strftime('%Y-%m-%d')}.csv"
-    folder = 'job_data'
-    key_name = f'{folder}/{current_date}'
-    df = read_csv_from_s3(bucket_name, key_name)
-    transform(df)
-
+    raw_folder = 'raw_job_data'
+    trans_folder = 'transformed_job_data'
+    raw_key_name = f'{raw_folder}/{current_date}'
+    trans_key_name = f'{trans_folder}/{current_date}'
+    df = read_csv_from_s3(bucket_name, raw_key_name)
+    trans_df = transform(df)
+    write_to_s3(trans_df, bucket_name, trans_key_name)
+    
 
 if __name__ == '__main__':
     main()
